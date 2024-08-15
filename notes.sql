f0183d0b9ea1152eedbf42326ed0296c7524cc8e
Jonhatan <jonyvilla44@gmail.com>
2024-08-14 20:34:00 -0600

    notes.sql file edited

--- notes.sql
+++ notes.sql
@@ -4,5 +4,3 @@ CREATE TABLE notes (
 	content TEXT,
 	created_at timestamp default current_timestamp
 );
-
-INSERT INTO notes (title, description) VALUES('React', 'Como crear un app full stack en react');
